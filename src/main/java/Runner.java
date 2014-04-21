3a33a0d890ea524dc3fae6c99f017c065f4c5c27
ks33 <kevin_shu@brown.edu>
2014-04-21 11:17:02 -0400

    New Stuff
    Dynamically load pages

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -12,26 +12,29 @@ import weka.core.Instances;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.*;
+
+import org.apache.velocity.VelocityContext;
 
 
 public class Runner {
 	public static Classifier MyClassifier;
     
 	public static void main(String[] args) throws JsonSyntaxException, MalformedURLException, IOException {
-		// 1) Change WekaProcessing main function's return type to Classifer, change name?
-		// 2) Make function return classifier
-		// c = (new WekaProcessing()).train()
-		// 3) create a new POST route (JsonTransformer)
-		// 			in this function, parse JSON from request body
-		//          create Instance() out of it (weka class)
-		//			pass it into c.classifyInstance()
-		//			create a Java class similar to MyMessage that will have instance variables for the attributes
-		//			of your response
-		//			return an instance of this class to generate a response
-		//	handle in client-side javascript
+		final List<String> attributeFields = new ArrayList<>();
+		final List<String> attributeFieldType = new ArrayList<>();
+		final String finalAttributeType;
+		final String finalAttribute;
+		final List<String> possibilities;
+		final List<Attribute> attributeListData = new ArrayList<>();
+	
+		staticFileLocation("/assets");
+		//org.apache.log4j.BasicConfigurator.configure();
+		 
 		
         try {
         	MyClassifier = (new WekaProcessing()).train();
@@ -40,8 +43,37 @@ public class Runner {
 			e.printStackTrace();
 		}
 		
-		
-		
+        Scanner sc = null;
+		try {
+			sc = new Scanner(new FileReader("outputFile.arff"));
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		String line = sc.nextLine();
+		while(!line.equals("@data"))
+		{
+			if(line.startsWith("@attribute"))
+			{
+				String[] lineSplit = line.split(" (?=(([^'\']*['\']){2})*[^'\']*$)");
+				for (int i = 0; i < lineSplit.length; i++)
+				{
+					lineSplit[i] = lineSplit[i].replace("'", "");
+				}
+				attributeFields.add(lineSplit[1]);
+				attributeFieldType.add(lineSplit[2]);
+			}
+			line = sc.nextLine();
+		}
+		sc.close();
+		//these are the final value, not something we display on the page.
+		finalAttribute = attributeFields.get(attributeFields.size()-1);
+		String attributeType = attributeFieldType.get(attributeFieldType.size()-1);
+		possibilities = Arrays.asList(attributeType.substring(1, attributeType.length()-2).split(","));
+		attributeFields.remove(attributeFields.size()-1);
+		attributeFieldType.remove(attributeFieldType.size()-1);
+
 		// TODO Auto-generated method stub
 		get(new Route("/") {
             @Override
@@ -50,17 +82,47 @@ public class Runner {
             }
         });
 		
-		/*get(new JsonTransformerRoute("/hello") {
-         @Override
-         public Object handle(Request request, Response response) {
-         return new MyMessage("Hello World");
-         }
-		 });*/
-		
+		get(new VelocityRoute("/DataFileUpload")
+		{
+			@Override
+			public Object handle(final Request request, final Response response)
+			{
+                Map<String, Object> model = new HashMap<>();
+
+				return modelAndView(model, "uploadFile.wm");
+			}
+	});
+		get(new VelocityRoute("/makeTrainingSet")
+		{
+			@Override
+			public Object handle(final Request request, final Response reponse)
+			{
+				Map<String, Object> model = new HashMap<>();
+				Scanner sc = null;
+				try {
+					sc = new Scanner(new FileReader("data1.csv"));
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				String[] fields = sc.nextLine().split(",");
+				List<List<String>> values = new ArrayList<List<String>>();
+				while (sc.hasNextLine())
+				{
+					String s = sc.nextLine();
+					values.add(Arrays.asList(s.split(",")));
+				}
+				model.put("Fields", fields);
+				model.put("Values", values);
+				return modelAndView(model, "viewData.wm");
+			}
+		});
 		get(new VelocityRoute("/hello1") {
             @Override
             public Object handle(final Request request, final Response response) {
                 Map<String, Object> model = new HashMap<>();
+              
+			    model.put("tokens", attributeFields);
                 model.put("hello", "Velocity World");
                 model.put("m", new MyMessage("hi bro"));
                 // The wm files are located under the resources directory
@@ -75,78 +137,43 @@ public class Runner {
 
                 Map<String, Object> model = new HashMap<>();
 		    	Gson gson = new Gson();
-			    DataObject mydata = gson.fromJson(request.body(), DataObject.class);
+		    	System.out.println(request.body());
+			    DataObject mydata = new DataObject(request.body());
+			    VelocityContext context = new VelocityContext();
+			    
+			  
 		    	System.out.println(mydata.toString());
 		    	/* Creating an instance with our values */
 		    	//declare attributes
-		    	Attribute tenure = new Attribute("tenure");
-		    	Attribute is30dPayer = new Attribute("is30dPayer");
-		    	Attribute paymentAmount30d = new Attribute("paymentAmount30d");
-		    	Attribute engmtLastWk = new Attribute("engmtLastWk");
-		    	Attribute engmtWk_1 = new Attribute("engmtWk-1");
-		    	Attribute engmtWk_2 = new Attribute("engmtWk-2");
-		    	Attribute engmtWk_3 = new Attribute("engmtWk-3");
-		    	Attribute engmt28d = new Attribute("engmt28d");
-		    	Attribute deltaEngmt4wk = new Attribute("deltaEngmt4wk");
-		    	Attribute socialActionsReceivedLastWk = new Attribute("socialActionsReceivedLastWk");
-		    	Attribute socialActorsReceivedLastWk = new Attribute("socialActorsReceivedLastWk");
-		    	Attribute socialActionsSentLastWk = new Attribute("socialActionsSentLastWk");
-		    	Attribute socialActorsSentLastWk = new Attribute("socialActorsSentLastWk");
-		    	Attribute daysSinceLastSend = new Attribute("daysSinceLastSend");
-		    	Attribute daysSinceLastReceive = new Attribute("daysSinceLastReceive");
-		    	FastVector attributeList = new FastVector(15);
-                
-	            /*ArrayList<String> classVal = new ArrayList<String>();
-                 classVal.add("ClassA");
-                 classVal.add("ClassB");*/
-	            
-	            FastVector fvNominalVal = new FastVector(2);
-	            fvNominalVal.addElement("non-lapser");
-	            fvNominalVal.addElement("lapser");
-	            Attribute attribute1 = new Attribute("trueLapse", fvNominalVal);
-	            
-		    	attributeList.addElement(tenure);
-		    	attributeList.addElement(is30dPayer);
-		    	attributeList.addElement(paymentAmount30d);
-		    	attributeList.addElement(engmtLastWk);
-		    	attributeList.addElement(engmtWk_1);
-		    	attributeList.addElement(engmtWk_2);
-		    	attributeList.addElement(engmtWk_3);
-		    	attributeList.addElement(engmt28d);
-		    	attributeList.addElement(deltaEngmt4wk);
-		    	attributeList.addElement(socialActionsReceivedLastWk);
-		    	attributeList.addElement(socialActorsReceivedLastWk);
-		    	attributeList.addElement(socialActionsSentLastWk);
-		    	attributeList.addElement(socialActorsSentLastWk);
-		    	attributeList.addElement(daysSinceLastSend);
-		    	attributeList.addElement(daysSinceLastReceive);
+		    	FastVector attributeList = new FastVector(attributeFields.size());
+	            FastVector fvNominalVal = new FastVector(possibilities.size());
+	            Attribute attribute1 = new Attribute(finalAttribute, fvNominalVal);
+
+
+	            for(String elem : possibilities)
+	            	fvNominalVal.addElement(elem);
+
+		    	for (String elem : attributeFields)
+		    	{
+		    		Attribute attr = new Attribute(elem);
+		    		attributeList.addElement(attr);
+		    		attributeListData.add(attr);
+		    	}
 		    	attributeList.addElement(attribute1);
-		    	
-                
-		    	Instance inst = new Instance(15);
+
+		    	Instance inst = new Instance(attributeFields.size());
 		        
-		    	Instances data = new Instances("TestInstances",attributeList,15);
+		    	Instances data = new Instances("TestInstances",attributeList,attributeFields.size());
 		    	data.setClassIndex(data.numAttributes()-1);
                 
+		    	for (int i = 0; i < attributeListData.size(); i ++)
+		    	{
+		    		//if()
+					inst.setValue(attributeListData.get(i), Double.valueOf(mydata.getData(attributeListData.get(i).toString().split(" ")[1])));
+		    	}
 		    	//set attributes into instance
 		    	System.out.println("The instance: " + inst);
-		    	System.out.println("tenure data: " + mydata.gettenure());
-		    	inst.setValue(tenure, mydata.gettenure());
-		    	System.out.println("=====");
-		    	inst.setValue(is30dPayer, mydata.getis30dPayer());
-		    	inst.setValue(paymentAmount30d, mydata.getpaymentAmount30d());
-		    	inst.setValue(engmtLastWk, mydata.getengmtLastWk());
-		    	inst.setValue(engmtWk_1, mydata.getengmtWk_1());
-		    	inst.setValue(engmtWk_2, mydata.getengmtWk_2());
-		    	inst.setValue(engmtWk_3, mydata.getengmtWk_3());
-		    	inst.setValue(engmt28d, mydata.getengmt28d());
-		    	inst.setValue(deltaEngmt4wk, mydata.getdeltaEngmt4wk());
-		    	inst.setValue(socialActionsReceivedLastWk, mydata.getsocialActionsReceivedLastWk());
-		    	inst.setValue(socialActorsReceivedLastWk, mydata.getsocialActorsReceivedLastWk());
-		    	inst.setValue(socialActionsSentLastWk, mydata.getsocialActionsSentLastWk());
-		    	inst.setValue(socialActorsSentLastWk, mydata.getsocialActorsSentLastWk());
-		    	inst.setValue(daysSinceLastSend, mydata.getdaysSinceLastSend());
-		    	inst.setValue(daysSinceLastReceive, mydata.getdaysSinceLastReceive());
+
 		    	System.out.println("The instance: " + inst);
 		    	System.out.println("=====");
 		    	
@@ -161,17 +188,8 @@ public class Runner {
 					e.printStackTrace();
 				}
 		    	response.type("application/json");
-				return new MyMessage(""+result);
+				return new MyMessage(finalAttribute + ": " + possibilities.get((int)result));
 		    }
         });
-		
-//		get(new JsonTransformerRoute("/result") {
-//			@Override
-//			public Object handle(Request request, Response response) {
-//				response.type("application/json");
-//				return new MyMessage(Double.toString(result));
-//			}
-//			
-//		});
 	}
 }
--- src/main/java/WekaProcessing.java
+++ src/main/java/WekaProcessing.java
@@ -38,39 +38,6 @@ public class WekaProcessing {
         System.out.println("Precision is: " + eval.precision(1));
         return myClassifier;
         
-        /*   // generate curve
-	     ThresholdCurve tc = new ThresholdCurve();
-	     int classIndex = 0;
-	     Instances result = tc.getCurve(eval.predictions(), classIndex);
-         
-	     // plot curve
-	     ThresholdVisualizePanel vmc = new ThresholdVisualizePanel();
-	     vmc.setROCString("(Area under ROC = " +  Utils.doubleToString(tc.getROCArea(result), 4) + ")");
-	     vmc.setName(result.relationName());
-	     PlotData2D tempd = new PlotData2D(result);
-	     tempd.setPlotName(result.relationName());
-	     tempd.addInstanceNumberAttribute();
-	     // specify which points are connected
-	     boolean[] cp = new boolean[result.numInstances()];
-	     for (int n = 1; n < cp.length; n++)
-         cp[n] = true;
-	     tempd.setConnectPoints(cp);
-	     // add plot
-	     vmc.addPlot(tempd);
-         
-	     // display curve
-	     String plotName = vmc.getName();
-	     final javax.swing.JFrame jf =
-         new javax.swing.JFrame("Weka Classifier Visualize: "+plotName);
-	     jf.setSize(500,400);
-	     jf.getContentPane().setLayout(new BorderLayout());
-	     jf.getContentPane().add(vmc, BorderLayout.CENTER);
-	     jf.addWindowListener(new java.awt.event.WindowAdapter() {
-         public void windowClosing(java.awt.event.WindowEvent e) {
-         jf.dispose();
-         }
-	     });
-	     jf.setVisible(true);*/
     }
     
 }
